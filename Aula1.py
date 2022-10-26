e6e7f0b122f6a0cc11591efc438f943432b948c6
Leandro Rodrigues Silva Miranda <105825495+Leandro-rodrigues-silva-miranda@users.noreply.github.com>
2022-10-26 19:25:17 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,15 @@
+#Hello World, para ganhar a bençao
+print("hello World")
+
+#Declaração de variaveis
+nome = "Leandro"
+idade = 21
+
+#Impressão de variaveis
+print(nome)
+print(idade,"\n")
+
+#print formatado
+
+print(f"O nome é "+nome)
+print(f"A idade é {idade}")
